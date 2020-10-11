53dfa7e2261496ee31e2b4080acc86f68d9f8206
kahenya-anita <anitakahenya1@gmail.com>
2020-10-11 21:04:34 +0300

    Import models to views

--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,61 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import numpy as np
+from .models import Image, Category, Location
  
 def home(request):
-    return render(request, 'home.html')
+    images = Image.objects.all()
+    print(images[0].category)
+    arr= np.array(images) 
+    newarr = np.array_split(arr,3)
+    first = newarr[0]
+    second = newarr[1]
+    third = newarr[2]
+    locations = Location.objects.all()
+    categories = Category.objects.all()
+    return render(request, 'home.html', {"first": first,"second": second,"third": third,"locations": locations,"categories": categories})
+
+def image(request, image_id):
+    try:
+        image = Image.objects.get(id=image_id)
+        print(image.category.id)
+    except ObjectDoesNotExist:
+        message = "Image does not exist or may have been deleted!"
+        return render(request, 'image.html', {"message":message})
+    return render(request, 'image.html', {"image":image})
+
+def category(request, category_id):
+    try:
+        category = Category.objects.get(id = category_id)
+        images = Image.search_image(category)
+        arr= np.array(images) 
+        newarr = np.array_split(arr,3)
+        first = newarr[0]
+        second = newarr[1]
+        third = newarr[2]
+        message = category.name
+        title = category.name
+        return render(request, 'search.html',{"title":title, "message":message,"images": images,"first": first,"second": second,"third": third})
+    except ObjectDoesNotExist:
+        message = "NO ITEMS UNDER CATEGORY " + search.upper()
+        categories = Category.objects.all()
+        title= "Not Found"
+        return render(request, 'search.html',{"title":title,"message":message, "categories": categories})
+
+def location(request, location_id):
+    try:
+        location = Location.objects.get(id=location_id)
+        images = Image.filter_by_location(location)
+        arr= np.array(images) 
+        newarr = np.array_split(arr,3)
+        first = newarr[0]
+        second = newarr[1]
+        third = newarr[2]
+        message = location.name
+        title = location.name
+        return render(request, 'search.html',{"title":title, "message":message,"images": images,"first": first,"second": second,"third": third})
+    except ObjectDoesNotExist:
+        message = "NO ITEMS FOR THAT LOCATION"
+        locations = Location.objects.all()
+        title= "Not Found"
+        return render(request, 'search.html',{"title":title,"message":message, "locations": locations})
