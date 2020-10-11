5f5e84be95cc5d37f6081f4ea6a9d77ed179fc52
kahenya-anita <anitakahenya1@gmail.com>
2020-10-11 12:27:44 +0300

    Templating

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse
+import numpy as np
+ 
+def home(request):
+    return render(request, 'home.html')
