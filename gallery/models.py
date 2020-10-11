dfe32b58c392535086126792826f50b4779cc31d
kahenya-anita <anitakahenya1@gmail.com>
2020-10-11 20:58:31 +0300

    Add category class

--- gallery/models.py
+++ gallery/models.py
@@ -1,5 +1,20 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
 
+class Category(models.Model):
+    name = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return self.name
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+    @classmethod
+    def update_category(cls,id,name):
+        cls.objects.filter(id = id).update(name = name)
 
 
