d740e70af371029bf92d8c1bf93be42cb96c13b5
kahenya-anita <anitakahenya1@gmail.com>
2020-10-12 02:06:32 +0300

    Register cloudinary

--- pictures/settings.py
+++ pictures/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
+import cloudinary
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -41,6 +42,12 @@ INSTALLED_APPS = [
     'bootstrap4',
 ]
 
+cloudinary.config( 
+  cloud_name = os.environ.get('CLOUD_NAME'), 
+   api_key = os.environ.get('API_KEY'), 
+  api_secret = os.environ.get('API_SECRET') 
+  )
+  
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
