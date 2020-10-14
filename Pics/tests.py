49944ae14229fa9d37bf3c99ede1ce7e1027c1e0
anita-that-chiq <62019551+kahenya-anita@users.noreply.github.com>
2020-10-14 12:03:35 +0300

    Update tests.py

--- Pics/tests.py
+++ Pics/tests.py
@@ -1,3 +1,210 @@
 from django.test import TestCase
+import tempfile
+from .models import Location, Category, Image
+from PIL import Image as Picture
+from django.test import TestCase
+from django.test import override_settings
 
-# Create your tests here.
+class CategoryTest(TestCase):
+    def setUp(self):
+        '''
+        creates new instances before a test
+        '''
+        self.destinations = Category(name = "destinations")
+        self.art = Category(name = "art")
+
+    def tearDown(self):
+        '''
+        clears database after each test
+        '''
+        Category.objects.all().delete()
+
+    def test_category_instance(self):
+        '''
+        test whether the new locations are an instance of the Location class
+        '''
+        self.assertTrue(isinstance(self.destinations, Category))
+        self.assertTrue(isinstance(self.art, Category))
+
+    def test_save_category_method(self):
+        '''
+        test whether new locations are added to the db 
+        '''
+        self.destinations.save_category()
+        self.art.save_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories) == 2)
+
+    def test_delete_category(self):
+        '''
+        tests whether category is deleted
+        '''
+        self.destinations.save_category()
+        self.art.save_category()
+        categories1 = Category.objects.all()
+        self.assertEqual(len(categories1),2)
+        self.destinations.delete_category()
+        categories2 = Category.objects.all()
+        self.assertEqual(len(categories2),1)
+
+    def test_update_category(self):
+        '''
+       tests whether the category name is updated
+        '''
+        self.destinations.save_category()
+        self.destinations.update_category(self.nature.id,'travel')
+        self.destinations.update_category(self.destinations.id,'travel')
+        update = Category.objects.get(name = "travel")
+        self.assertEqual(update.name, 'travel')
+
+def get_temporary_image(temp_file):
+        size = (200, 200)
+        color = (255, 0, 0, 0)
+        image = Picture.new("RGBA", size, color)
+        image.save(temp_file, 'png')
+        return temp_file
+
+def get_temporary_image2(temp_file2):
+        size = (200, 200)
+        color = (255, 0, 0, 0)
+        image = Picture.new("RGBA", size, color)
+        image.save(temp_file2, 'png')
+        return temp_file2
+
+class ImageTestClass(TestCase):
+    def setUp(self):
+        '''
+        creates new instances before a test
+        '''
+        self.nature = Category( name= "nature")
+        self.destinations = Category( name= "destinations")
+        self.nakuru = Location (name = "Nakuru")
+        self.flower = Image(photo = "image.png", name ='maua', description = 'beautiful', category= self.destinations, location= self.nakuru)
+
+    def tearDown(self):
+        '''
+        clears database after each test
+        '''
+        Image.objects.all().delete()
+        Category.objects.all().delete()
+        Location.objects.all().delete()
+
+    def test_image_instance(self):
+        '''
+        test whether the new image created is an instance of the Image class
+        '''
+        self.assertTrue(isinstance(self.flower, Image))
+
+    @override_settings(MEDIA_ROOT=tempfile.gettempdir())
+    def test_save_image(self):
+        '''
+        test whether new image is added to the db 
+        '''
+        self.art.save_category()
+        self.nakuru.save_location()
+        temp_file = tempfile.NamedTemporaryFile()
+        test_image = get_temporary_image(temp_file)
+        #test_image.seek(0)
+        self.test = Image(photo = test_image.name, name ='test', description = 'test image', category= self.destinations, location= self.nakuru )
+        self.test.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images)> 0)
+
+    @override_settings(MEDIA_ROOT=tempfile.gettempdir())
+    def test_display_images(self):
+        '''
+        test whether we can display images from the db
+        '''
+        self.art.save_category()
+        self.destinations.save_category()
+        self.nakuru.save_location()
+        temp_file = tempfile.NamedTemporaryFile()
+        test_image = get_temporary_image(temp_file)
+
+        temp_file = tempfile.NamedTemporaryFile()
+        test_image = get_temporary_image(temp_file)
+        #test_image.seek(0)
+        self.test = Image(photo = test_image.name, name ='test', description = 'test image', category= self.destinations, location= self.nakuru )
+        self.test.save_image()
+        images = Image.display_all_images()
+        self.assertEqual(len(images), 1)
+
+    @override_settings(MEDIA_ROOT=tempfile.gettempdir())
+    def test_delete_images(self):
+        '''
+        test whether image is deleted
+        '''
+        self.destinations.save_category()
+        self.nakuru.save_location()
+        temp_file = tempfile.NamedTemporaryFile()
+        test_image = get_temporary_image(temp_file)
+        #test_image.seek(0)
+        self.test = Image(photo = test_image.name, name ='test', description = 'test image', category= self.destinations, location= self.nakuru )
+        self.test.save_image()
+        images1 = Image.objects.all()
+        self.assertEqual(len(images1),1)
+        self.test.delete_image()
+        images2 = Image.objects.all()
+        self.assertEqual(len(images2),0)
+
+    @override_settings(MEDIA_ROOT=tempfile.gettempdir())
+    def test_update_image(self):
+        '''
+        test whether the image description is updated
+        '''
+        self.destinations.save_category()
+        self.nakuru.save_location()
+        temp_file = tempfile.NamedTemporaryFile()
+        test_image = get_temporary_image(temp_file)
+        self.test = Image(photo = test_image.name, name ='test', description = 'test image', category= self.destinations, location= self.nakuru )
+        self.test.save_image()
+
+        temp_file2 = tempfile.NamedTemporaryFile()
+        test_image2 = get_temporary_image2(temp_file2)
+        self.test2 = Image(photo = test_image2.name, name ='test', description = 'test image', category= self.destinations, location= self.nakuru )
+        self.test.update_image(self.test.id, self.test2.photo)
+        update = Image.objects.get(name = "test")
+        self.assertEqual(update.photo, self.test2.photo)
+
+    @override_settings(MEDIA_ROOT=tempfile.gettempdir())
+    def test_get_image_by_id(self):
+        '''
+        test whether image is retrieved by id 
+        '''
+        self.destinations.save_category()
+        self.nakuru.save_location()
+        temp_file = tempfile.NamedTemporaryFile()
+        test_image = get_temporary_image(temp_file)
+        self.test = Image(photo = test_image.name, name ='test', description = 'test image', category= self.destinations, location= self.nakuru )
+        self.test.save_image()
+        image = Image.get_image_by_id(self.test.id)
+        self.assertEqual(image.name, self.test.name)
+
+    @override_settings(MEDIA_ROOT=tempfile.gettempdir())
+    def test_search_image_by_category(self):
+        '''
+        Tests whether image is retrieved by category
+        '''
+        self.destinations.save_category()
+        self.nakuru.save_location()
+        temp_file = tempfile.NamedTemporaryFile()
+        test_image = get_temporary_image(temp_file)
+        self.test = Image(photo = test_image.name, name ='test', description = 'test image', category= self.destinations, location= self.nakuru )
+        self.test.save_image()
+        images = Image.search_image("destinations")
+        self.assertTrue(len(images) > 0)
+
+    @override_settings(MEDIA_ROOT=tempfile.gettempdir())
+    def test_search_location(self):
+        '''
+        Tests whether image is retrieved by location
+        '''
+        self.destinations.save_category()
+        self.nakuru.save_location()
+        temp_file = tempfile.NamedTemporaryFile()
+        test_image = get_temporary_image(temp_file)
+        self.test = Image(photo = test_image.name, name ='test', description = 'test image', category= self.destinations, location= self.nakuru )
+        self.test.save_image()
+        images = Image.filter_by_location("Nakuru")
+        self.assertTrue(len(images) > 0)
+
